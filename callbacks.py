de3c614dd8c0da5fb39e7fefb092eb77cb14316a
Invactive <gkuba8@gmail.com>
2023-06-06 16:11:23 +0200

    Changed UI and added ClearGraphs callback

--- callbacks.py
+++ callbacks.py
@@ -1,6 +1,12 @@
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 import layout
 import dash
+import importlib
+import plotly.express as px
+import pandas as pd
+
+importlib.reload(layout)
 
 
 def get_callbacks(app):
@@ -9,7 +15,7 @@ def get_callbacks(app):
         [Input('controllersRB', 'value')]
     )
     def update_output(value):
-        return layout.drawParams(value)
+        return layout.drawControllerParams(value)
 
     @app.callback(
         Output('outputPar1', 'children'),
@@ -70,3 +76,96 @@ def get_callbacks(app):
         trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
         value = inp_value if trigger_id == "inpParamTd" else slider_value
         return value, value
+
+    @app.callback(
+        [Output("graph0", "figure"),
+         Output("graph1", "figure"),
+         Output("graph2", "figure"),
+         Output("graph3", "figure")],
+        Input('clear-button', 'n_clicks')
+    )
+    def clear_graphs(n_clicks):
+        if n_clicks is None:
+            raise PreventUpdate
+        else:
+            return (px.line().update_layout(
+                template='plotly_dark',
+                plot_bgcolor='rgba(0, 0, 0, 0)',
+                paper_bgcolor='rgba(0, 0, 0, 0)',
+                title={
+                    'text': '<b>' + "Speed v in time t" + '</b>',
+                    'x': 0.5,
+                    'y': 0.95,
+                    'xanchor': 'center',
+                    'yanchor': 'top',
+                    "font_color": "#aaaaaa",
+                    'font': {
+                        'size': 20
+                    }
+                },
+                modebar={
+                    'orientation': 'v',
+                    'bgcolor': 'rgba(0,0,0,0.5)'
+                }
+            ),
+                px.line().update_layout(
+                template='plotly_dark',
+                plot_bgcolor='rgba(0, 0, 0, 0)',
+                paper_bgcolor='rgba(0, 0, 0, 0)',
+                title={
+                    'text': '<b>' + "Position x in time t" + '</b>',
+                    'x': 0.5,
+                    'y': 0.95,
+                    'xanchor': 'center',
+                    'yanchor': 'top',
+                    "font_color": "#aaaaaa",
+                    'font': {
+                        'size': 20
+                    }
+                },
+                modebar={
+                    'orientation': 'v',
+                    'bgcolor': 'rgba(0,0,0,0.5)'
+                }
+            ),
+                px.line().update_layout(
+                template='plotly_dark',
+                plot_bgcolor='rgba(0, 0, 0, 0)',
+                paper_bgcolor='rgba(0, 0, 0, 0)',
+                title={
+                    'text': '<b>' + "Error e in time t" + '</b>',
+                    'x': 0.5,
+                    'y': 0.95,
+                    'xanchor': 'center',
+                    'yanchor': 'top',
+                    "font_color": "#aaaaaa",
+                    'font': {
+                        'size': 20
+                    }
+                },
+                modebar={
+                    'orientation': 'v',
+                    'bgcolor': 'rgba(0,0,0,0.5)'
+                }
+            ),
+                px.line().update_layout(
+                template='plotly_dark',
+                plot_bgcolor='rgba(0, 0, 0, 0)',
+                paper_bgcolor='rgba(0, 0, 0, 0)',
+                title={
+                    'text': '<b>' + "Control signal u in time t" + '</b>',
+                    'x': 0.5,
+                    'y': 0.95,
+                    'xanchor': 'center',
+                    'yanchor': 'top',
+                    "font_color": "#aaaaaa",
+                    'font': {
+                        'size': 20
+                    }
+                },
+                modebar={
+                    'orientation': 'v',
+                    'bgcolor': 'rgba(0,0,0,0.5)'
+                }
+            )
+            )
--- layout.py
+++ layout.py
@@ -1,6 +1,5 @@
 from dash import Dash, dcc, html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
 import plotly.express as px
 import json
 import pandas as pd
@@ -8,10 +7,11 @@ import pandas as pd
 
 def drawSlider(text: str):
     return html.Div(
-        className="row-container",
+        className="row-container-" + text,
         children=[
             html.Div(text + ":",
-                     style={"margin-top": "-5px"}),
+                     style={"margin-top": "-5px",
+                            "width": "25px"}),
             dcc.Slider(
                 id="slider" + text,
                 className="slider",
@@ -34,12 +34,13 @@ def drawSlider(text: str):
         ])
 
 
-def drawFigure(df: pd.DataFrame, f_title: str):
+def drawFigure(df: pd.DataFrame, f_title: str, id: int):
     return html.Div([
         dbc.Card(
             dbc.CardBody([
                 dcc.Graph(
-                    figure=px.line(df, x="x", y="y", title=f_title
+                    id="graph" + str(id),
+                    figure=px.line(df, x="x", y="y", title=f_title,
                                    ).update_layout(
                         template='plotly_dark',
                         plot_bgcolor='rgba(0, 0, 0, 0)',
@@ -66,7 +67,7 @@ def drawFigure(df: pd.DataFrame, f_title: str):
     ])
 
 
-def drawText(text: str):
+def drawMainHeader(text: str):
     return html.Div([
         dbc.Card(
             dbc.CardBody([
@@ -78,70 +79,118 @@ def drawText(text: str):
     ])
 
 
-def drawParams(controller: str):
+def drawSimParams():
+    return html.Div([html.H2("Simulation Parameters",
+                             style={'textAlign': 'center'}
+                             ),
+                     dbc.Row(
+        className="row-container-Sim",
+        children=[
+            html.Div("Simulation Time [s]" + ":",
+                     style={"margin-top": "-5px",
+                            "width": "150px"}),
+            dcc.Slider(
+                id="sliderSimTime",
+                className="sliderSim",
+                min=0,
+                max=1000,
+                value=0,
+                step=1,
+                marks=None,
+                tooltip={"placement": "bottom",
+                         "always_visible": True},
+            ),
+            dcc.Input(
+                id="inpParamSimTime",
+                className="inputBox",
+                placeholder='Enter a value...',
+                type='number',
+                value=0,
+                style={"margin-top": "-5px"},
+            ),
+
+        ]),
+    ])
+
+
+def drawButtons():
+    return html.Div(
+        className="btns-container",
+        children=[
+            html.Button("Clear Graphs",
+                        id="clear-button",
+                        className="btn"),
+            html.Button("Update Graphs",
+                        id="apply-button",
+                        className="btn"),
+        ])
+
+
+def drawControllerParams(controller: str):
+    layout = None
     if controller == "P":
-        return html.Div([html.H2("Controller Parameters",
-                                 style={'textAlign': 'center'}
-                                 ),
-                         drawSlider("Kp"),
-                         html.Div(
-            html.Button("Update Graphs",
-                        id="apply-button",
-                        className="btn"),
-            style={"text-align": "center"}
-        ),
-            html.Div(
-            html.Button("Clear Graphs",
-                        id="clear-button",
-                        className="btn"),
-            style={"text-align": "center"}
-        )
-        ])
+        layout = html.Div([html.H2("Controller Parameters",
+                                   style={'textAlign': 'center'}
+                                   ),
+                           drawSlider("Kp"),
+                           ])
     if controller == "PI":
-        return html.Div([html.H2("Controller Parameters",
-                                 style={'textAlign': 'center'}
-                                 ),
-                         drawSlider("Kp"),
-                         html.Br(),
-                         drawSlider("Ti"),
-                         html.Div(
-            html.Button("Update Graphs",
-                        id="apply-button",
-                        className="btn"),
-            style={"text-align": "center"}
-        ),
-            html.Div(
-            html.Button("Clear Graphs",
-                        id="clear-button",
-                        className="btn"),
-            style={"text-align": "center"}
-        )
-        ])
+        layout = html.Div([html.H2("Controller Parameters",
+                                   style={'textAlign': 'center'}
+                                   ),
+                           drawSlider("Kp"),
+                           html.Br(),
+                           drawSlider("Ti"),
+                           ])
     if controller == "PID":
-        return html.Div([html.H2("Controller Parameters",
-                                 style={'textAlign': 'center'}
-                                 ),
-                         drawSlider("Kp"),
-                         html.Br(),
-                         drawSlider("Ti"),
-                         html.Br(),
-                         drawSlider("Td"),
-                         html.Div(
-            html.Button("Update Graphs",
-                        id="apply-button",
-                        className="btn"),
-            style={"text-align": "center"}
-        ),
-            html.Div(
-            html.Button("Clear Graphs",
-                        id="clear-button",
-                        className="btn"),
-            style={"text-align": "center"}
-        )
-        ])
+        layout = html.Div([html.H2("Controller Parameters",
+                                   style={'textAlign': 'center'}
+                                   ),
+                           drawSlider("Kp"),
+                           html.Br(),
+                           drawSlider("Ti"),
+                           html.Br(),
+                           drawSlider("Td"),
+                           ])
     if controller == "FUZZY":
-        return html.Div([html.H2("Controller Parameters", style={'textAlign': 'center'}),
-                        html.H3("FUZZY PH", style={'textAlign': 'center'})])
+        layout = html.Div([html.H2("Controller Parameters", style={'textAlign': 'center'}),
+                           html.H3("FUZZY PH", style={'textAlign': 'center'})])
+
+    if layout != None:
+        return layout, drawButtons()
+
+
+def drawModelParams():
+    return html.Div([html.H2("Model Parameters",
+                             style={'textAlign': 'center'}
+                             ),
+                     html.Div(
+        className="row-container-Sim-Tp",
+        children=[
+            html.Div("Model Param PH" + ":",
+                     style={"margin-top": "-5px",
+                            "width": "150px"}),
+            dcc.Slider(
+                id="sliderModelTime",
+                className="sliderSim",
+                min=0,
+                max=1000,
+                value=0,
+                step=1,
+                marks=None,
+                tooltip={"placement": "bottom",
+                         "always_visible": True},
+            ),
+            dcc.Input(
+                id="inpParamSimTime",
+                className="inputBox",
+                placeholder='Enter a value...',
+                type='number',
+                value=0,
+                style={"margin-top": "-5px"},
+            ),
+        ]),
+    ])
 
 
 # Data
@@ -154,23 +203,23 @@ df = pd.DataFrame(dict(
 ))
 fig = px.line(df, x="x", y="y", title="Position x in time t")
 
-df2 = pd.DataFrame(dict(
+df1 = pd.DataFrame(dict(
     x=DATA["t"],
     y=DATA["x"]
 ))
-fig2 = px.line(df2, x="x", y="y", title="Position x in time t")
+fig1 = px.line(df1, x="x", y="y", title="Position x in time t")
 
-df3 = pd.DataFrame(dict(
+df1 = pd.DataFrame(dict(
     x=DATA["t"],
     y=DATA["e"]
 ))
-fig3 = px.line(df3, x="x", y="y", title="Position x in time t")
+fig2 = px.line(df1, x="x", y="y", title="Position x in time t")
 
-df4 = pd.DataFrame(dict(
+df3 = pd.DataFrame(dict(
     x=DATA["t"],
     y=DATA["u"]
 ))
-fig4 = px.line(df4, x="x", y="y", title="Position x in time t")
+fig3 = px.line(df3, x="x", y="y", title="Position x in time t")
 
 
 def create_layout():
@@ -180,47 +229,52 @@ def create_layout():
             dbc.CardBody([
                 dbc.Row([
                     dbc.Col([
-                        drawText("Control Panel")
+                        drawMainHeader("Control Panel")
                     ], width=12),
                 ], align='center'),
+                dbc.Row(className="params-container",
+                        children=[
+                            dbc.Col([
+                                drawSimParams(),
+                            ], width=4),
+                            dbc.Col([
+                                html.Div([html.H2("Controllers", style={'textAlign': 'center'}),
+                                          dcc.RadioItems(
+                                    style={'textAlign': 'center'},
+                                    id='controllersRB',
+                                    options=[
+                                        {'label': ' P Controller', 'value': 'P'},
+                                        {'label': ' PI Controller', 'value': 'PI'},
+                                        {'label': ' PID Controller', 'value': 'PID'},
+                                        {'label': ' Fuzzy Logic Controller',
+                                            'value': 'FUZZY'}
+                                    ]
+                                ),
+                                ], style={'textAlign': 'left'}),
+                                html.Br(),
+                                html.Div(id="controllerParams", style={
+                                    'textAlign': 'center'}),
+                            ], width=4),
+                            dbc.Col([
+                                drawModelParams(),
+                            ], width=4),
+                            dbc.Row([
+                                dbc.Col([
+                                    drawFigure(df, "Speed v in time t", 0)
+                                ], width=3),
+                                dbc.Col([
+                                    drawFigure(df1, "Position x in time t", 1)
+                                ], width=3),
+                                dbc.Col([
+                                    drawFigure(df1, "Error e in time t", 2)
+                                ], width=3),
+                                dbc.Col([
+                                    drawFigure(
+                                        df3, "Control signal u in time t", 3)
+                                ], width=3),
+                            ], align='center'),
+                        ]),
                 html.Br(),
-                dbc.Row([
-                    dbc.Col([
-                        html.Div([html.H2("Controllers", style={'textAlign': 'center'}),
-                                  dcc.RadioItems(
-                            style={'textAlign': 'center'},
-                            id='controllersRB',
-                            options=[
-                                {'label': ' P Controller', 'value': 'P'},
-                                {'label': ' PI Controller', 'value': 'PI'},
-                                {'label': ' PID Controller', 'value': 'PID'},
-                                {'label': ' Fuzzy Logic Controller', 'value': 'FUZZY'}
-                            ]
-                        ),
-                        ], style={'textAlign': 'left'}),
-                        html.Br(),
-                        html.Br(),
-                        html.Div(id="controllerParams", style={
-                                 'textAlign': 'center'}),
-                    ], width=4, align='start'),
-                    dbc.Col([
-                        drawFigure(df, "Speed v in time t")
-                    ], width=4),
-                    dbc.Col([
-                        drawFigure(df2, "Position x in time t")
-                    ], width=4),
-                ]),
-                html.Br(),
-                dbc.Row([
-                    dbc.Col([
-                    ], width=4, align='start'),
-                    dbc.Col([
-                        drawFigure(df3, "Error e in time t")
-                    ], width=4),
-                    dbc.Col([
-                        drawFigure(df4, "Control signal u in time t")
-                    ], width=4),
-                ], align='center'),
             ]), color='dark'
         )
     ])
